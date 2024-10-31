81008f40b6eaebe3146b53f28c2c95761ffaa9bb
YGADS <3284134319@qq.com>
2024-10-31 23:25:49 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i = 0;
+    for (int i = 0, j = 10;i = j = 10;i++, j--);
+    printf("%d", i);
+    return 0;
+
+}
